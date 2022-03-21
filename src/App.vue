3412dfb5ccf637fd794cd565ea398d796e0acd6e
jscasca <jscasca@gmail.com>
2022-03-21 16:20:42 +1000

    Test page

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,63 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <ul id='channels'>
+    <li v-for='channel in channels' :key='channel.value'>
+      <div >
+        <a :href='baseUrl + "?channel=" + channel.value'>{{ channel.name }}</a>
+      </div>
+    </li>
+  </ul>
+  <tiny-editor :channel="channel" :conf="conf.classic" :snippet="snippets.basic" title="Classic Editor" />
+  <tiny-editor :channel="channel" :conf="conf.inline" :snippet="snippets.basic" title="Inline Editor" />
+  <tiny-editor :channel="channel" :conf="conf.quickbars" :snippet="snippets.basic" title="Quickbars Editor" />
+  <tiny-editor :channel="channel" :conf="conf.bottom" :snippet="snippets.basic" title="Bottom Toolbar Editor" />
+  <tiny-editor :channel="channel" :conf="conf.resize" :snippet="snippets.full" title="Resize Editor" />
+  <tiny-editor :channel="channel" :conf="conf.template" :snippet="snippets.template" title="Template Editor" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { basic, full, template } from './settings/settings.ts';
+import { classic, inline, quickbars, bottom, resize, templateConf } from './settings/configurations.ts';
+import TinyEditorVue from './components/TinyEditor.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    'tiny-editor': TinyEditorVue
+  },
+  setup() {
+    const baseUrl = window.location.href.indexOf('?') > 0 ? window.location.href.substring(0, window.location.href.indexOf('?')) : window.location.href;
+    const params = new URLSearchParams(window.location.search);
+    const channel = params.get('channel') || '6-dev';
+    const conf = {
+      classic,
+      inline,
+      quickbars,
+      bottom,
+      resize,
+      template: templateConf
+    }
+
+    const snippets = {
+      basic,
+      full,
+      template
+    };
+
+    const channels = [
+    { name: '6 Development', value: '6-dev' },
+    { name: '6 Testing', value: '6-testing' },
+    { name: '6 Stable', value: '6-stable' },
+    { name: '5 Stable', value: '5' }
+    ];
+
+    return {
+      channels,
+      baseUrl,
+      channel,
+      conf,
+      snippets
+    }
   }
 }
 </script>
---
+++ src/components/TinyEditor.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h2>{{ title }}</h2>
+    <editor :api-key="apiKey" :init="conf" :initialValue="snippet" />
+  </div>
+</template>
+<script>
+import Editor from '@tinymce/tinymce-vue';
+
+export default {
+  name: 'TinyEditor',
+  props: {
+    title: String,
+    conf: Object,
+    snippet: String
+  },
+  components: { editor: Editor },
+  setup() {
+    const apiKey = 'b1g4d59rwwqxx1vj7mci23rjj8ubgb46i4xsio6ieig6fkps';
+    return {
+      apiKey,
+    };
+  },
+}
+</script>
